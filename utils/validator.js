5961f933a535c6f8c28106350994a66b8ad2c039
adremone <20bcs105@iiitdwd.ac.in>
2023-04-22 11:18:46 +0530

    validation added

--- utils/validator.js
+++ utils/validator.js
@@ -6,12 +6,14 @@ const validatePassword = (password)=>{
     return hasUppercase && hasLowercase && hasDigit;
 }
 
-const validateUrl = ()=>{
+const validateUrl = (url)=>{
     const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9\-]+(?:\.[a-z0-9\-]+)+[/?#][^\s]*$/i;
+    return urlRegex.test(url)
 }
 
-const validateEmail = ()=>{
+const validateEmail = (email)=>{
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    return emailRegex.test(email)
 } 
 
 module.exports = {validateEmail,validatePassword,validateUrl}
